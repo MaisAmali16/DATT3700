1edeef73bcd17b8c01e7c0f8bab4690341fbadab
Cecilia <cece02@my.yorku.ca>
2026-03-11 16:26:13 -0400

    Text w/ effect
    Dialogue added with Typewriter Effect

---
+++ Assets/TypewriterEffect.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+using TMPro;
+
+public class TypewriterEffect : MonoBehaviour
+{
+    private TextMeshProUGUI tmProText;
+    public float delay = 0.1f;
+
+    void Start()
+    {
+        tmProText = GetComponent<TextMeshProUGUI>();
+        if (tmProText != null)
+        {
+            tmProText.maxVisibleCharacters = 0;
+            StartCoroutine(TypeCharacters());
+        }
+    }
+
+    IEnumerator TypeCharacters()
+    {
+        tmProText.ForceMeshUpdate();
+        int totalVisibleCharacters = tmProText.textInfo.characterCount;
+
+        for (int i = 0; i <= totalVisibleCharacters; i++)
+        {
+            tmProText.maxVisibleCharacters = i;
+            yield return new WaitForSeconds(delay);
+        }
+    }
+}
