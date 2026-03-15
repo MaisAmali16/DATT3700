9fef4b562cde745a63ba04ec2dcd6455b8d9d96f
MaisAmali16 <maisamali9898@gmail.com>
2026-03-15 03:11:00 -0400

    Added spatial audio and door audio

--- Assets/Scripts/SecondRoom/DoorTrigger.cs
+++ Assets/Scripts/SecondRoom/DoorTrigger.cs
@@ -3,13 +3,18 @@ using UnityEngine;
 public class Room2Trigger : MonoBehaviour
 {
     public Room2Manager room2Manager;
-
+    public AudioSource leftDoorAudio;  // Slot for Left Door
+    public AudioSource rightDoorAudio; // Slot for Right Door
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("MainCamera") || other.CompareTag("Player"))
         {
             Debug.Log("Player entered Room 2 trigger");
             room2Manager.StartRoom();
+            
+            // Play both sounds at once!
+            if(leftDoorAudio != null) leftDoorAudio.Play();
+            if(rightDoorAudio != null) rightDoorAudio.Play();
         }
     }
 }
